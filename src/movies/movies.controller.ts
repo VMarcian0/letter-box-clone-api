d74c8cbe2ab493158feed3272c3058f1bf9c2ee0
vmarcian0 <victor_marcian0@hotmail.com>
2022-08-13 17:03:51 -0300

    almost there - for real

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { map } from 'rxjs';
+import { combineLatest, map, Observable } from 'rxjs';
 import { MovieDBService } from '../services/moviedb/moviedb.service';
+import { MovieDbMovieType } from '../services/moviedb/types/movie.type';
+import { MovieDbPaginationType } from '../services/moviedb/types/pagination.type';
 import { MoviesByGenreDto } from './dto/movies-by-genre.dto';
 
 @Controller('movies')
@@ -16,10 +18,50 @@ export class MoviesController {
 
     @Get('/byGenre')
     getMovieByGenre(@Query() queryParams:MoviesByGenreDto){
+        
+        /**
+         * @TODO ajustar o slice para condizer com o que foi pedido
+         */
+        
         const defaultPageSize = 20;
         const pageSize = +queryParams?.pageSize || defaultPageSize;
         const pageNumber = +queryParams?.page || 1;
         const pageNumberToGet = Math.ceil(pageSize*pageNumber/20);
+        /**
+         * Supondo um array com todos os retornos possiveis,
+         * esse objeto definiria qual é o intervalo desse array que a api precisar retornar
+         */
+        const slice = {
+            start: (pageNumber -1)*pageSize,
+            end: (pageSize * pageNumber)
+        }
+        const pagesToFetch = {
+            start: Math.ceil(slice.start/defaultPageSize)+1,
+            end: Math.ceil(slice.end/defaultPageSize)
+        }
+        let observables : Observable<MovieDbPaginationType<MovieDbMovieType>>[] = [];
+        for(let page = pagesToFetch.start; page<= pagesToFetch.end; page++){
+            observables.push(this.movieDBService.getMoviesByGenre(queryParams.themes_ids, page))
+        }
+        console.log('slice',slice)
+        console.log('pagesToFetch',pagesToFetch)
+
+        return combineLatest(observables)
+            .pipe(
+                map( (result) => {
+                    let results : MovieDbMovieType[] = []  
+                    result.map((data )=> {results = [...results, ...data.results]});
+                    return {
+                        'total': result[0].total_results,
+                        'total_pages': Math.ceil(result[0].total_results/pageSize),
+                        'page': pageNumber,
+                        'results': results.slice(
+                            slice.start-pageSize,
+                            slice.end-pageSize
+                        )
+                    }
+                })
+            )
         /**
          * @Todo
          * Encadear mais de uma chamada para poder funcionar com qualquer page size
@@ -38,8 +80,8 @@ export class MoviesController {
                         'total_pages': Math.ceil(data.total_results/pageSize),
                         'page': pageNumber,
                         'results': data.results.slice(
-                            (pageNumber -1)*pageSize,
-                            (pageSize * pageNumber)
+                            0,
+                            pageSize //gambiara
                         )
                     }
                 })
